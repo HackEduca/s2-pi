c2847f5b9a997476e49a43147fde0da877bdd211
Edson Sidnei Sobreira <5200448+sobreira@users.noreply.github.com>
2019-04-13 13:52:35 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 import os
 from subprocess import call
 
-# call(["pip3", "install", "git+https://github.com/dpallot/simple-websocket-server.git"])
-# call(["pip3", "install", "git+https://github.com/giampaolo/psutil.git"])
+call(["pip3", "install", "git+https://github.com/dpallot/simple-websocket-server.git"])
+call(["pip3", "install", "git+https://github.com/giampaolo/psutil.git"])
 
 user = os.listdir("/home")
 pth = '/home/' + user[0]
@@ -11,26 +11,19 @@ pth = '/home/' + user[0]
 # call(["wget", "-P", pth, "https://raw.githubusercontent.com/HackEduca/s2-pi/master/s2_pi/s2_pi.js"])
 
 setup(
-<<<<<<< HEAD
-    name='s2-pi_ptbr',
-    version='2.0',
-    packages=['s2_pi_ptbr'],
-=======
     name='s2-pi',
-    version='1.9',
+    version='2.0',
     packages=['s2_pi'],
->>>>>>> parent of 54ea900... Update setup.py
 
     entry_points={
-            'console_scripts': ['s2pi_ptbr = s2_pi_ptbr.s2_pi:run_server',
+            'console_scripts': ['s2pi = s2_pi.s2_pi:run_server',
                                 'sbx_to_sb2 = s2_pi.sbx_to_sb2:sbx_to_sb2'],
         },
     url='https://github.com/HackEduca/s2-pi',
     license='GNU General Public License v3 (GPLv3)',
-    author='Alan Yorinks - changed by Edson Sidnei Sobreira',
-    author_email='MisterYsLab@gmail.com - changed by hackeduca@hackeduca.com.br',
+    author='Alan Yorinks',
+    author_email='MisterYsLab@gmail.com',
     description='Creating Scratch 2 Extensions For The Raspberry Pi',
-    include_package_data=True,
     keywords=['Raspberry Pi', 'Scratch 2', 'Extensions'],
         classifiers=[
             'Development Status :: 5 - Production/Stable',
